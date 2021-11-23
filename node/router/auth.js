db80bed1fb2a3646e9ae0f413f0b677813f16b8a
fanliu <gebilaofl@163.com>
2021-11-23 17:52:28 +0800

    fix: eslint 报错

--- node/router/auth.js
+++ node/router/auth.js
@@ -1,5 +1,5 @@
-import Auth from '../controllers/auth'
-export = (router) => {
-  // TODO:router对象封装   
-  router.post(process.env.API_BASE + "login", Auth.login);
-}
+const Auth = require('../controllers/auth');
+module.exports = (router) => {
+  // TODO:router对象封装
+  router.post(process.env.API_BASE + 'login', Auth.login);
+};
